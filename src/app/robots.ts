f2fe150e9a917181b42619450be247fa7de8e98f
OQTutor Developer <imrankhang3920@gmail.com>
2026-07-17 12:45:02 +0500

    Disallow cgi-bin directory inside robots.txt configuration

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/admin/', '/api/', '/_next/'],
+      disallow: ['/admin/', '/api/', '/_next/', '/cgi-bin/'],
     },
     sitemap: 'https://www.oqtutor.com/sitemap.xml',
   };
