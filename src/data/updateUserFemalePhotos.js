88ca9b5591bde2410ebaf690f60d51b66dad7a1e
OQTutor Developer <imrankhang3920@gmail.com>
2026-07-23 01:08:01 +0500

    Update profile pictures for Qari Imran, Ustadha Maryam, Qaria Sadia, Ustadha Zainab, Qaria Bushra and Ustadha Hina

--- src/data/updateUserFemalePhotos.js
+++ src/data/updateUserFemalePhotos.js
@@ -13,7 +13,12 @@ const uploads = [
   { name: "Qaria Sumaira Younis", src: "media__1784745563390.jpg", filename: "qaria_sumaira_younis.jpg" },
   { name: "Ustadha Aiman Shafeeq", src: "media__1784737530660.jpg", filename: "ustadha_aiman_shafeeq.jpg" },
   { name: "Ustadha Sumaira Khan", src: "media__1784737530749.jpg", filename: "ustadha_sumaira_khan.jpg" },
-  { name: "Qaria Rimshah Noor", src: "media__1784737530895.jpg", filename: "qaria_rimshah_noor.jpg" }
+  { name: "Qaria Rimshah Noor", src: "media__1784737530895.jpg", filename: "qaria_rimshah_noor.jpg" },
+  { name: "Ustadha Maryam Siddiqui", src: "media__1784749879871.jpg", filename: "ustadha_maryam_siddiqui.jpg" },
+  { name: "Qaria Sadia Parveen", src: "media__1784749879949.jpg", filename: "qaria_sadia_parveen.jpg" },
+  { name: "Ustadha Zainab Bibi", src: "media__1784749880320.png", filename: "ustadha_zainab_bibi.png" },
+  { name: "Qaria Bushra Rashid", src: "media__1784749880439.jpg", filename: "qaria_bushra_rashid.jpg" },
+  { name: "Qari Muhammad Imran", src: "media__1784749880615.jpg", filename: "qari_muhammad_imran.jpg" }
 ];
 
 uploads.forEach(item => {
@@ -27,6 +32,14 @@ uploads.forEach(item => {
   }
 });
 
+// Copy default placeholder to Ustadha Hina Malik
+const defaultFemaleSrc = path.join(__dirname, '../../public/tutor-female-icon.jpg');
+const HinaMalikDest = path.join(publicTutorsDir, 'ustadha_hina_malik.jpg');
+if (fs.existsSync(defaultFemaleSrc)) {
+  fs.copyFileSync(defaultFemaleSrc, HinaMalikDest);
+  console.log(`Copied default placeholder tutor-female-icon.jpg -> ustadha_hina_malik.jpg`);
+}
+
 // Update db.json
 const dbPath = path.join(__dirname, 'db.json');
 const dbData = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
@@ -36,6 +49,9 @@ dbData.tutors.forEach(tutor => {
   if (match) {
     tutor.photo = `/tutors/${match.filename}`;
     console.log(`Updated photo for ${tutor.name} to /tutors/${match.filename}`);
+  } else if (tutor.name === "Ustadha Hina Malik") {
+    tutor.photo = `/tutors/ustadha_hina_malik.jpg`;
+    console.log(`Updated photo for ${tutor.name} to /tutors/ustadha_hina_malik.jpg`);
   }
 });
 
