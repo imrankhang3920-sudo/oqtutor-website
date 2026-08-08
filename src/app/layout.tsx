82427783cc07fd3d663fe33307efd81289ec2f51
OQTUTOR <imrankhang3920@gmail.com>
2026-08-08 07:46:55 +0500

    Add second Meta Pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -116,7 +116,7 @@ export default function RootLayout({
           }}
         />
 
-        {/* Meta Pixel Code */}
+        {/* Meta Pixel Code 1 */}
         <script
           id="meta-pixel"
           dangerouslySetInnerHTML={{
@@ -140,6 +140,34 @@ export default function RootLayout({
             width="1"
             style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=1196171388446085&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
+        {/* Meta Pixel Code 2 */}
+        <script
+          id="meta-pixel-2"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1276384067814550');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1276384067814550&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
